4c50add2dadda089ec4f46fccb56bd11d0358d2e
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-13 11:37:01 +0100

    feat: Closure per salutare

--- exercise.js
+++ exercise.js
@@ -1,29 +1,19 @@
-/*
-const sum = function (a, b) {
-  return a + b;
+function greet(greeting) {
+  return function(nome) {
+    return `${greeting}, ${nome}`;
+  }
 }
 
-const subtract = function (a, b) {
-  return a - b;
-}
+const greetWithHello = greet('Hello');
+const greetWithBye = greet('Bye');
+const greetWithHi = greet('Hi');
 
-const multiply = function (a, b) {
-  return a * b;
-}
+const greetPaulWithHello = greetWithHello('Paul');
+const greetPaulWithHi = greetWithHi('Paul');
+const greetJohnWithBye = greetWithBye('John');
+const greetSimonWithHello = greetWithHello('Simon');
 
-const divide = function (a, b) {
-  return a / b;
-}
-
-const log = function (value) {
-  console.log(value);
-}
-*/
-
-const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide   = (a, b) => a / b;
-const log = value => console.log(value);
-// Potrebbe essere <log(`((2 + 4) * (5 + 2) - 2) / 5`);> ma a noi piacciono le sfide, pertanto:
-log(`((${subtract(1970, 1968)} + ${multiply(8, 0.5)}) * (${divide(35.5, 7.1)} + ${sum(-137.17062001, 139.17062001)}) - ${subtract(2, 0)}) / ${multiply(0.2, 25)}`);
+console.log(greetPaulWithHello);
+console.log(greetPaulWithHi);
+console.log(greetJohnWithBye);
+console.log(greetSimonWithHello);
