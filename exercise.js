9fe37e34aedb63501aa4a1ac9e0c3cf3af32a24e
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-13 13:40:25 +0100

    feat: Closure per creare un negozio

--- exercise.js
+++ exercise.js
@@ -1,18 +1,24 @@
-function pow(base) {
-  return function(esponente) {
-    return base ** esponente;
-  }
+function createStore() {
+  const assortimentoo = []; /* Riecco la doppia lettera finale. Ebbene, laddove non si vorrà nominare un array con l'entità del singolo elemento declinata al plurale
+                            (es. in questo caso sarebbe stato "articoli") ma si preferirà utilizzare un termine esprimente collettività, tale termine, che verrà declinato al
+                            singolare, avrà appunto la doppia finale per convenzionalmente (©opyright: Stefano Assogna) indicare che è riferito a un array */
+  return function(articolo) {
+    assortimentoo.push(articolo);
+    assortimentoo.forEach(ele => {
+      console.log(`Item #${ele.id}: ${ele.name}`);
+    });
+  }    
 }
 
-const pow2 = pow(2);
-const pow5 = pow(5);
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
 
-console.log('--- Power of 2 ---');
-console.log(pow2(4));
-console.log(pow2(2));
-console.log(pow2(8));
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
 
-console.log('--- Power of 5 ---');
-console.log(pow5(4));
-console.log(pow5(2));
-console.log(pow5(8));
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
