03d3fd8fcf83bbacd31fb55aedf2cc364767201c
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-13 12:46:20 +0100

    feat: Closure per elevare a potenza

--- exercise.js
+++ exercise.js
@@ -1,19 +1,18 @@
-function greet(greeting) {
-  return function(nome) {
-    return `${greeting}, ${nome}`;
+function pow(base) {
+  return function(esponente) {
+    return base ** esponente;
   }
 }
 
-const greetWithHello = greet('Hello');
-const greetWithBye = greet('Bye');
-const greetWithHi = greet('Hi');
+const pow2 = pow(2);
+const pow5 = pow(5);
 
-const greetPaulWithHello = greetWithHello('Paul');
-const greetPaulWithHi = greetWithHi('Paul');
-const greetJohnWithBye = greetWithBye('John');
-const greetSimonWithHello = greetWithHello('Simon');
+console.log('--- Power of 2 ---');
+console.log(pow2(4));
+console.log(pow2(2));
+console.log(pow2(8));
 
-console.log(greetPaulWithHello);
-console.log(greetPaulWithHi);
-console.log(greetJohnWithBye);
-console.log(greetSimonWithHello);
+console.log('--- Power of 5 ---');
+console.log(pow5(4));
+console.log(pow5(2));
+console.log(pow5(8));
