fd2bdc231100b4b2f4ef0fc491cc08b163ec5ddf
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-18 11:29:28 +0100

    feat: Clonazione oggetto

--- exercise.js
+++ exercise.js
@@ -4,12 +4,12 @@ const person1 = {
   age: 25
 };
 
-const person2 = person1;
-person2.firstName = "Simon";
+// Modifica la linea sottostante affinché venga creata e assegnata a person2 
+// una copia esatta di person1, permettendo così di modificare il "firstName" di person2,
+// senza che venga modificato anche il valore "firstName" di person1
+const person2 = Object.assign({}, person1);
 
-// Modifica la proprietà "firstName" di person2 in "Simon"
+person2.firstName = 'Simon';
 
-console.log(person1); /* NB: Anche qui sarà "Simon". Si è fatto coincidere l'indirizzo dello stesso oggetto per entrambe le variabili, NO copiato l'oggetto in modo svincolato ovvero per
-                        far vivere l'oggetto in modo indipendente per ciascuna variabile. */
-
+console.log(person1);
 console.log(person2);
