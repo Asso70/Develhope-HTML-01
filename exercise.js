b3e0541088a6921c1c591b3b32b84777671a1546
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-18 09:34:03 +0100

    feat: Closure per (auto-)chiamata ricorsiva

--- exercise.js
+++ exercise.js
@@ -1,13 +1,28 @@
-function canPlay() {
-  const personName = 'Paul';
-
-  if (true) {
-    personName = 'George'; // Argh! Voglio che personName sia una variabile di sola lettura
-  }
-
-  personName += ' plays football';
-
-  console.log(personName);
+function memoize(fn) {
+  let cache = {numeri: [], risultati: []};
+  return function(numero) {
+    for(let i = 0; i < cache.numeri.length; i = i + 1) {
+      if(cache.numeri[i] === numero) {
+        console.log(`Fetching from cache for ${numero}`)
+        return cache.risultati[i];
+      }
+    }
+    console.log(`Calculating result for ${numero}`);
+    let risultato = fn(numero);
+    cache.numeri.push(numero);
+    cache.risultati.push(risultato);
+    return risultato;
+  };
 }
-
-canPlay();
+    
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
+    
+factorial = memoize(factorial);
+console.log(factorial(10));
+console.log(factorial(6));
+console.log(factorial(5));
