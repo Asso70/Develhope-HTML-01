f5c4bf37a871e69c5a30d98633c4212f8d66c912
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-19 14:03:46 +0100

    feat: Creazione oggetti con costruttore

--- exercise.js
+++ exercise.js
@@ -1,28 +1,11 @@
-const person = {
-  firstName: "",
-  lastName: "",
-  getFirstName: function() {
-    return this.firstName;
-  },
-  getLastName: function() {
-    return this.lasttName;
-  },
-  setFirstName: function(nome) {
-    this.firstName = nome;
-  },
-  setLastName: function(nome) {
-    this.lastName = nome;
-  },
-  fullName: function() {
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
-let john = Object.assign({}, person);
-john.setFirstName("John");
-john.setLastName("Doe");
-let simon = Object.assign({}, person);
-simon.setFirstName("Simon");
-simon.setLastName("Collins");
-
+let john = new Person("John", "Doe");
+let simon = new Person("Simon", "Collins");
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
