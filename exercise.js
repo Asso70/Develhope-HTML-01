715dafc3270f8035ca04604f46dde82c3d2c3719
Stefano Assogna <stefano.assogna@bulgari.com>
2022-01-19 13:27:50 +0100

    feat: Creazione e modifica oggetti

--- exercise.js
+++ exercise.js
@@ -1,10 +1,28 @@
 const person = {
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "",
+  lastName: "",
+  getFirstName: function() {
+    return this.firstName;
+  },
+  getLastName: function() {
+    return this.lasttName;
+  },
+  setFirstName: function(nome) {
+    this.firstName = nome;
+  },
+  setLastName: function(nome) {
+    this.lastName = nome;
+  },
+  fullName: function() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
+let john = Object.assign({}, person);
+john.setFirstName("John");
+john.setLastName("Doe");
+let simon = Object.assign({}, person);
+simon.setFirstName("Simon");
+simon.setLastName("Collins");
 
-// Print values of person using Object.keys
-Object.keys(person).forEach(function(ele) {
-  console.log(`${ele}: ${person[ele]}`);
-});
+console.log(john.fullName()); // John Doe
+console.log(simon.fullName()); // Simon Collins
