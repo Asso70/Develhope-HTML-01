80442a1d945e7205b4ffa37b13c93fbc90fcb985
Stefano Assogna <stefano.assogna@bulgari.com>
2022-02-02 17:28:46 +0100

    feat: Instanziamento

--- exercise.js
+++ exercise.js
@@ -1,28 +1,10 @@
-function memoize(fn) {
-  let cache = {numeri: [], risultati: []};
-  return function(numero) {
-    for(let i = 0; i < cache.numeri.length; i = i + 1) {
-      if(cache.numeri[i] === numero) {
-        console.log(`Fetching from cache for ${numero}`)
-        return cache.risultati[i];
-      }
-    }
-    console.log(`Calculating result for ${numero}`);
-    let risultato = fn(numero);
-    cache.numeri.push(numero);
-    cache.risultati.push(risultato);
-    return risultato;
-  };
-}
-    
-function factorial(x) {
-  if (x === 0) {
-    return 1;
+// Class definition
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  return x * factorial(x - 1);
 }
-    
-factorial = memoize(factorial);
-console.log(factorial(10));
-console.log(factorial(6));
-console.log(factorial(5));
+
+const developer = new Person('Mario', 'Rossi');
+console.log(`${developer.firstName} ${developer.lastName}`);
