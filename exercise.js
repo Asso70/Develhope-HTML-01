9bf14746a3099539486d5311d1d8640f4becab49
Stefano Assogna <stefano.assogna@bulgari.com>
2022-02-03 15:51:20 +0100

    feat: Classe Conto in Banca

--- exercise.js
+++ exercise.js
@@ -1,17 +1,22 @@
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+class BankAccount {
+  constructor(totAmount) {
+    this.totAmount = totAmount;
   }
-  static fromObject(object) {
-    return object;
+
+  deposit(amount) {
+    this.totAmount = this.totAmount + amount;
+  }
+
+  withdraw(amount) {
+    this.totAmount = this.totAmount - amount;
+  }
+
+  view() {
+    console.log(`Total amount: ${this.totAmount}`)
   }
 }
-
-const obj = {
-  firstName: 'Mario',
-  lastName: 'Rossi'
-};
-
-const person = Person.fromObject(obj);
-console.log(`${person.firstName} ${person.lastName}`);
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(800);
+bankAccount.view();
