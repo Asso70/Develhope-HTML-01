dba2826deb78d27b944be51bebe0c7828b5ea192
Stefano Assogna <stefano.assogna@bulgari.com>
2022-02-04 11:45:28 +0100

    feat: Metodi getter e setter

--- exercise.js
+++ exercise.js
@@ -1,42 +1,45 @@
-class Square {
-  constructor(side) {
-    this.side = side;
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.fullName = this.getFullName();
+  }
+
+// I prossimi 6 metodi non sono necessari essendo i membri (o le proprietà) pubblici dunque direttamente accessibili in lettura e scrittura
+  getFirstName() {
+    return this.firstName;
+  }
+
+  getLastName() {
+    return this.lastName;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  setFirstName(firstName) {
+    this.firstName = firstName;
+  }
+
+  setLastName(lastName) {
+    this.lastName = lastName;
+  }
+
+  setAge(age) {
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
-class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-}
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
 
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
-  }
-}
-
-class AreaCalculator {
-  static calculate(geometricShape) {
-    if(geometricShape instanceof Square) {
-      return geometricShape.side ** 2;
-    }
-    else if(geometricShape instanceof Rectangle) {
-      return geometricShape.width * geometricShape.height;
-    }
-    else if(geometricShape instanceof Circle) {
-      return Math.PI * geometricShape.radius ** 2;
-    }
-    else return `${geometricShape} is an undefined geometric shape`;
-  }
-}
-
-const square = new Square(4);
-const rectangle = new Rectangle(4, 2);
-const circle = new Circle(5);
-
-console.log(AreaCalculator.calculate(square));
-console.log(AreaCalculator.calculate(rectangle));
-console.log(AreaCalculator.calculate(circle));
-console.log(AreaCalculator.calculate({a: "A", b: "B"}));
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+// Qui si sarebbe dovuto chiamare il metodo getter ma immagino la traccia degli esercizi non vada cambiata
+console.log(person.fullName);
