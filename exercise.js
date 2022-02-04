394a5013ddaecdeb9254f870587838ed07bd324d
Stefano Assogna <stefano.assogna@bulgari.com>
2022-02-04 16:36:37 +0100

    feat: Classi custom errore

--- exercise.js
+++ exercise.js
@@ -7,29 +7,22 @@ class BankAccount {
 
   deposit(amount) {
     if (amount < 0) {
-      throw new Error('The amount provided cannot be negative');
+      throw new NegativeAmountError('The amount provided cannot be negative'); // Use custom Error class
     }
 
     this.#amount += amount;
   }
 
   withdraw(amount) {
-  // Nell'esercizio precedente davo per scontato di dover evitare il crash. E ho messo try-catch su entrambi i metodi di modifica conto (pur avendo in realtà prima
-  // testato che andasse in crash senza i try-catch). OK, cmq ora geatisco su questo (sull'altro sarebbe identico a come già fatto nell'esercizio precedente)
-    try {
-      if (amount < 0) {
-        throw new Error('The amount provided cannot be negative');
-      }
-
-      if (this.#amount < amount) {
-        throw new Error('You cannot withdraw more than account balance');
-      }
-
-      this.#amount -= amount;
-      }
-    catch(e) {
-      console.log(e.message);
+    if (amount < 0) {
+      throw new NegativeAmountError('The amount provided cannot be negative'); // Use custom Error class
     }
+
+    if (this.#amount < amount) {
+      throw new WithdrawNotPermittedError('You cannot withdraw more than account balance'); // Use custom Error class
+    }
+
+    this.#amount -= amount;
   }
 
   view() {
@@ -37,9 +30,16 @@ class BankAccount {
   }
 }
 
-// Handle errors to avoid app crash
-const bankAccount = new BankAccount(1000);
-bankAccount.deposit(500);
-bankAccount.deposit(200);
-bankAccount.withdraw(10000);
-bankAccount.view();
+try {
+  const bankAccount = new BankAccount(1000);
+  bankAccount.deposit(500);
+  bankAccount.deposit(200);
+  bankAccount.withdraw(10000);
+  bankAccount.view();
+} catch (e) {
+  console.log('Something went wrong during bank account operations');
+  
+}
+
+class NegativeAmountError extends Error {};
+class WithdrawNotPermittedError extends Error {};
