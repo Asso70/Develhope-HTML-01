7eb633e633381489afb008b74d6ace28158e7e87
Stefano Assogna <stefano.assogna@bulgari.com>
2022-02-10 12:53:20 +0100

    feat: Promises, promises...

--- exercise.js
+++ exercise.js
@@ -19,23 +19,56 @@ const persons = [
   }
 ];
 
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+// core here
 function fetchPersonById(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const person = persons.find(item => item.id === id);
-      
-      if (person) {
-        return resolve(JSON.stringify(person));
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      const person = persons.find(function(item) {
+        return item.id === id
+      });
+      if(person) {
+        return resolve(person);
       }
 
       return reject(`Person with id: ${id} doesn't exist`);
-    }, 1000);
-  });
+    }, 3000);
+  })
 }
 
-// core here
-fetchPersonById(2).then(function(person) {
-  return JSON.parse(person);
-}).then(function(person) {
-  console.log(person);
+function fetchJobById(id) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      const job = jobs.find(function(item) {
+        return item.id === id
+      });
+      if(job) {
+        return resolve(job);
+      }
+
+      return reject(`Job with id: ${id} doesn't exist`);
+    }, 1000);
+  })
+}
+
+function promises(id) { // Meglio avere un supervisore di chiamata ;-)
+  return [fetchPersonById(id), fetchJobById(id)];
+}
+
+Promise.all(promises(2)).then(function(results) {
+  console.log(results);
 });
