f1ab0114002aa3f385c2c5d314befbd932936b55
Stefano Assogna <stefano.assogna@bulgari.com>
2022-07-22 14:22:15 +0200

    fix: Exercise 1 write to console

--- Exercise01/script.mjs
+++ Exercise01/script.mjs
@@ -2,4 +2,5 @@ import { writeFile } from 'node:fs';
 
 writeFile("./Exercise01/textfile.txt", "Hello World!", (err) => {
   if(err) throw err;
+  console.log("Aw, right, file has been saved :-)");
 });
