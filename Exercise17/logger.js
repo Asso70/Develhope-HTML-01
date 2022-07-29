e7d4c95b798595881da21c9b6f0ab840f00fbee6
Stefano Assogna <stefano.assogna@bulgari.com>
2022-07-30 00:10:08 +0200

    feat: Exercise 17

---
+++ Exercise17/logger.js
@@ -0,0 +1,63 @@
+const fs = require("node:fs/promises");
+
+class Logger {
+  constructor(logStorage) {
+    this.logStorage = logStorage;
+  }
+
+  async info(message) {
+    await this.logStorage.write(`[INFO] ${message}\n`);
+  }
+
+  async error(message) {
+    await this.logStorage.write(`[ERROR] ${message}\n`);
+  }
+
+  async replay() {
+    console.log(await this.logStorage.read());
+  }
+}
+
+class LogStorageFSAdapter {
+  constructor(filepath) {
+    this.filepath = filepath;
+  }
+
+  async appendFile(message) {
+    try {
+      await fs.appendFile(this.filepath, `[INFO] ${message}\n`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async readFile() {
+    try {
+      return await fs.readFile(this.filepath, { encoding: "utf-8" });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async read() {
+    await this.readFile();
+  }
+
+  async write(message) {
+    await this.appendFile(message);
+  }
+}
+
+const fsStorage = new LogStorageFSAdapter("output.log");
+
+const logger = new Logger(fsStorage);
+
+logger.info("Some information");
+
+logger.error("A bit of an issue");
+
+logger.error("A catastrophic error!");
+
+logger.info("The best information");
+
+logger.replay();
