676ff8a380e2032eb191f6d5f144dc7c096727e5
Stefano Assogna <stefano.assogna@bulgari.com>
2022-08-01 11:41:22 +0200

    feat: Exercise 20

---
+++ Exercise20/server.mjs
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get("/", async (request, reply) => {
+  reply.send({data: "Up and running with Fastify!"});
+});
+
+try {
+  await fastify.listen({port: 3000});
+}
+catch(error) {
+  fastify.log.error(error);
+  process.exit(1);
+}
